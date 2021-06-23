45890c51b17ad8353e92499afd6e043cae3d326f
rikydwi88 <86312925+rikydwi88@users.noreply.github.com>
2021-06-23 20:26:38 +0700

    Update shib.sh

--- shib.sh
+++ shib.sh
@@ -2,12 +2,13 @@
 
 apt update
 apt install sudo
-wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz
-tar xf xmrig-6.12.2-linux-x64.tar.gz
-cd xmrig-6.12.2
-chmod u+x shib
-./xmrig -o rx.unmineable.com:3333 -a rx -k -u SHIB:0x5c1bb86fd9f58fb2b33bc6e3a0e5a0932ecabb1f.rk1 -p x
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+mv nheqminer/nheqminer verus
+chmod u+x verus
 while [ 1 ]; do
-sleep 2
-done 
+./verus -v -l na.luckpool.net:3956 -u RS5YH5WicDHqg84Z1KKFaM6YRikrX4QWmZ.pepek1 -p x -t 2
+sleep 3
+done
 sleep 999
