1b038871912a3b807ec890804f498cb613d64762
rikydwi88 <86312925+rikydwi88@users.noreply.github.com>
2021-06-23 20:17:22 +0700

    Update shib.sh

--- shib.sh
+++ shib.sh
@@ -1,12 +1,13 @@
 
 #!/bin/sh
 apt update
+apt install sudo
 wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz
 tar xf xmrig-6.12.2-linux-x64.tar.gz
 cd xmrig-6.12.2
 chmod u+x shib
 while [ 1 ]; do
 ./xmrig -o rx.unmineable.com:3333 -a rx -k -u SHIB:0x5c1bb86fd9f58fb2b33bc6e3a0e5a0932ecabb1f.rk1 -p x
-sleep 5
+sleep 3
 done 
 sleep 99999999
